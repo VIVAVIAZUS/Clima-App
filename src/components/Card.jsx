44520405bfa3e83ce568b2455578b163b777990a
VIVAVIAZUS <vivavgw@gmail.com>
2022-07-05 13:59:14 -0500

    create componet Card + MiniCardTemp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import MiniCardTemp from "./MiniCardTemp"
 
 export default function Card(props) {
-  // acá va tu código
-  return <div>Card Component</div>
+  // Compruebo si la la propiedad "onClose" contiene una funcion
+  function handleOnClose(){
+    console.log(typeof props.onClose);
+    if(typeof props.onClose==="function"){
+      props.onClose();
+    }
+  }
+
+  return <div>
+    <button onClick={handleOnClose}>x</button>
+    <h1>{props.name}</h1>
+    <MiniCardTemp temp="min" value={props.min}/>
+    <MiniCardTemp temp="max" value={props.min}/>
+    <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="Icono del clima" />
+  </div>
 };
---
+++ src/components/MiniCardTemp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MiniCardTemp(temp,value) {
+  return (
+    <div>
+        <span>{temp}</span>
+        <div>{value}</div>
+    </div>
+  )
+}
+
+export default MiniCardTemp;
